62cb8fe49249381c624d6ec43894b23f54bbf169
tkasha <ivan.tkachef@gmail.com>
2017-02-06 22:05:25 +0300

    Законченное приложение

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,8 +38,7 @@ class User < ApplicationRecord
   end
 
   def feed
-    # Это предварительное решение. См. полную реализацию в "Following users".
-    Micropost.where("user_id = ?", id)
+    Micropost.from_users_followed_by(self)
   end
 
   def following?(other_user)
